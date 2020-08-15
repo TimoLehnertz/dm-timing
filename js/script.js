9de887804232e1f77c98b9ec7c5ae8d4c4c95304
TimoLehnertz <timolehnertz1@gmail.com>
2020-08-16 01:07:58 +0200

    uptade

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ function change(){
     ersteRundeSek.value = parseFloat((gesSek.value / streckenlänge.value) * restMeter.value + startzugabe.value).toFixed(3);
 
     durchschittZeit.value = ((gesSek.value - ersteRundeSek.value) / runden.value).toFixed(3);
+
+    reset();
 }
 
 let lapsLeft = 3;
@@ -55,6 +57,22 @@ class Rennen{
         info += "</tr></table>"
         return info
     }
+
+    getLastTime(){
+        if(this.measurements.length == 0){
+            return 0;
+        }
+        let length = this.measurements.length;
+        return this.measurements[length - 1] - this.measurements[Math.max(0, length - 2)];
+    }
+
+    getCurrentTime(){
+        return this.measurements[this.measurements.length - 1] - this.measurements[0];
+    }
+
+    getLapsLeft(){
+        return this.runden - this.measurements.length + 1;
+    }
 }
 
 /*
@@ -65,40 +83,67 @@ function calcTip(rennen){
     for(let i = 0; i < rennen.measurements.length; i++){
         sum += rennen.measurements[i] - rennen.measurements[Math.max(0, i - 1)];
     }
-    let metersLeft = (rennen.streckenlänge - ((rennen.measurements.length - 1) * rennen.rundenlänge + rennen.restMeter));
+    //übrige meter nach drücken der Leertaste
+    //let metersLeft = (rennen.streckenlänge - (Math.max(0, rennen.measurements.length - 2) * rennen.rundenlänge) - (rennen.measurements.length > 1 ? rennen.restMeter : 0));
+
+    let timeLeft = (rennen.vorgabezeitMin * 60 * 1000 + rennen.vorgabezeitSek * 1000) - rennen.getCurrentTime();
+
+    let rundenLeft = rennen.getLapsLeft();
+
+    let tip = timeLeft / rundenLeft;
+
+    tip = tip - rennen.getLastTime();
+
+    if(isNaN(tip)){
+        tip = 0;
+    }
+
+    plusMinus.innerHTML = "<span>" + millisToSeconds(rennen.getLastTime()) + "</span><br>" +
+    (tip < 0 ? "-" : "+") + millisToSeconds(tip);
+    
+    if(tip < -100){
+        rundenÜbrig.style.background ="red";
+    } else if(tip > 300){
+        rundenÜbrig.style.background ="green";
+    } else{
+        rundenÜbrig.style.background ="gray";
+    }
 }
 
 function getSol(lap, rennen){
     return (lap < 2 ? rennen.vorgabeErsteRunde : rennen.vorgabeDurchschittRunde) * 1000;
 }
 
-function measurement(e){
+function keyDown(e){
     if(e.keyCode == 27){
         reset();
-    }
-    if(e.keyCode == 32){
-        //reset
-        if(!started){
-            start();
-        }
-        color = body.style.background;
-        body.style.background ="green";
-        
-        setTimeout(()=>{body.style.background="white";}, 200);
-        if(!(restMeter.value > 0 && measumentsTaken < 1)){
-            lapsLeft--;
-        }
-        jetztRennen.measurements.push(performance.now());
-        measumentsTaken++;
-        if(lapsLeft == -1){
-            finsish();
-        }
-        updateScreen();
-        calcTip(jetztRennen);
+    } else if(e.keyCode == 112){
+        measurement();
     }
 }
 
-function updateScreen(){
+function measurement(){
+    //reset
+    if(!started){
+        start();
+    }
+    color = body.style.background;
+    body.style.background ="green";
+    
+    setTimeout(()=>{body.style.background="#DDD";}, 200);
+    if(!(restMeter.value > 0 && measumentsTaken < 1)){
+        lapsLeft--;
+    }
+    jetztRennen.measurements.push(performance.now());
+    measumentsTaken++;
+    updateScreen();
+    calcTip(jetztRennen);
+    if(lapsLeft == -1){
+        finsish();
+    }
+}
+
+function updateScreen(){    
     rundenÜbrig.innerText = lapsLeft;
     showStats(jetztRennen, result);
 }
@@ -122,8 +167,8 @@ function start(){
     jetztRennen.date = Date.now();
     jetztRennen.streckenlänge = streckenlänge.value;
     jetztRennen.rundenlänge = rundenlänge.value;
-    jetztRennen.VorgabezeitMin = VorgabezeitMin.value;
-    jetztRennen.VorgabezeitSek = VorgabezeitSek.value
+    jetztRennen.vorgabezeitMin = VorgabezeitMin.value;
+    jetztRennen.vorgabezeitSek = VorgabezeitSek.value;
     jetztRennen.startZugabe = startzugabe.value;
     jetztRennen.vorgabeDurchschittRunde = durchschittZeit.value;
     jetztRennen.vorgabeErsteRunde = ersteRundeSek.value;
@@ -131,10 +176,10 @@ function start(){
     jetztRennen.runden = runden.value;
 }
 
-function finsish(){
+function finsish(){ 
     rennen.push(jetztRennen);
-    alert("Rennen gespeichert");
     reset();
+    alert("Rennen gespeichert");
 }
 
 function reset(){
@@ -143,33 +188,44 @@ function reset(){
     measumentsTaken = 0;
     lapsLeft = runden.value;
     updateScreen();
+    jetztRennen = new Rennen();
+    
 }
 
 function showStats(rennen, table){
     if(rennen == undefined){
         return;
     }
-    table.innerHTML = "<tr><td>Durchfahrt</td><td>Zeitpunkt</td><td>Zeit</td><td>sol</td><td>plus minus</td></tr>";
+    let innerHTML = "<tr><td>Durchfahrt</td><td>Zeitpunkt</td><td>Zeit</td><td>sol</td><td>plus minus</td></tr>";
     
     for(let i = 1; i - 1 < rennen.runden; i++){
         if(i < rennen.measurements.length){
-            let plus = (rennen.measurements[i] - rennen.measurements[Math.max(0, i - 1)]) - getSol(i, rennen);        
-            table.innerHTML += "<tr><td>" + i + "</td>"+
-            "<td>" + millisToMinutesAndSeconds(rennen.measurements[i] - rennen.measurements[0]) + "</td>"+
-            "<td>" + millisToMinutesAndSeconds(rennen.measurements[i] - rennen.measurements[Math.max(0, i - 1)]) + "</td>"+
-            "<td>" + millisToMinutesAndSeconds(getSol(i, rennen)) + "</td>"+
-            (plus < 0 ? "<td style='background-color: green;'>-" : "<td style='background-color: red;'>") + millisToMinutesAndSeconds(plus) + "</td></tr>";
+            let plus = (rennen.measurements[i] - rennen.measurements[Math.max(0, i - 1)]) - getSol(i, rennen);
+            innerHTML += "<tr><td>" + i + "</td>"+
+            "<td>" + millisToMinutesAndSeconds(rennen.measurements[i] - rennen.measurements[0]) + "</td>"+//Zeitpunkt
+            "<td>" + millisToMinutesAndSeconds(rennen.measurements[i] - rennen.measurements[Math.max(0, i - 1)]) + "</td>"+//zeit
+            "<td>" + millisToMinutesAndSeconds(getSol(i, rennen)) + "</td>"+//sol
+            (plus < 0 ? "<td style='background-color: green;'>-" : "<td style='background-color: red;'>") + millisToMinutesAndSeconds(plus) + "</td>"+
+            "</tr>";
         } else{
-            table.innerHTML += "<tr><td>" + i + "</td><td></td><td></td><td></td><td></td></tr>";
+            innerHTML += "<tr><td>" + i + "</td><td></td><td></td><td></td><td></td></tr>";
         }
     }
+    table.innerHTML = innerHTML;
 }
 
 function millisToMinutesAndSeconds(millis) {
     millis = Math.abs(millis);
     let minutes = Math.floor(millis / 60000);
-    let seconds = ((millis % 60000) / 1000).toFixed(0);
+    let seconds = parseInt(millis / 1000) % 60;
     return minutes + ":" + (seconds < 10 ? '0' : '') + seconds + ((millis % 1000) < 100 ? ((millis % 1000) < 10 ? ".00" : ".0") : ".") + parseInt(millis % 1000);
+}
+
+function millisToSeconds(millis) {
+    millis = Math.abs(millis);
+    let minutes = Math.floor(millis / 60000);
+    let seconds = parseInt(millis / 1000) % 60;
+    return (seconds < 10 ? '0' : '') + seconds + ((millis % 1000) < 100 ? ((millis % 1000) < 10 ? ".00" : ".0") : ".") + parseInt(millis % 1000);
 }
 
 function loadRace(raceId){
@@ -183,7 +239,19 @@ function loadRace(raceId){
     showStats(rennen[raceId], savedRaceTable);
 }
 
-body.onkeydown = measurement;
-reset.onkeydown = measurement;
+body.onkeydown = keyDown;
+savedRace.onmousedown = function (){
+    event.stopPropagation();
+}
+body.onscroll = scroll;
 
+function scroll(){
+    if(window.scrollY > 100){
+        title.classList.add("fadeOut");
+    } else{
+        title.classList.remove("fadeOut");
+    }
+}
 change();
+scroll();
+//alert(millisToMinutesAndSeconds());
